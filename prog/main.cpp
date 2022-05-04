221d68e545ddf85e2dddbce577aaefe9bc233302
farm26 <88470648+farm26@users.noreply.github.com>
2022-05-03 19:10:29 -0600

    Formato v.1

--- prog/main.cpp
+++ prog/main.cpp
@@ -1,47 +1,57 @@
-#include "seleccion.cpp"
-#include "Routing.cpp"
-#include <iostream>
+
+#include "Selection.cpp"
+#include "Router.cpp"
+#include "Generator.cpp"
+#include <math.h>
 #include <list>
-#include <algorithm>
-#include <cmath>
-#include <vector>
 #include <utility>
+#include <queue>
 
-using namespace std;
 
-int main()
-{
+
+void printSVG(tinyxml2::XMLDocument &doc, tinyxml2::XMLPrinter &printer) {
+	// Print in memory
+	doc.Print(&printer);
+	cout << printer.CStr() << endl;
+}
+
+int main() {
 
 	tinyxml2::XMLDocument doc;
 	tinyxml2::XMLPrinter printer;
-
-	doc.LoadFile("../arch_svg/wifi-1.svg");
-
-	int puntos[] = {2583, 79};
-	int colores[] = {0x000000};
-
-	seleccion Seleccion(puntos, 2, colores, 1, doc);
-	Seleccion.selectPaths();
-
-	vector<string> typePaths = Seleccion.getTypePath();
-	vector<float> limitsPaths = Seleccion.getPathsArea();
-	vector<vector<float>> valuesPaths = Seleccion.getPathValues();
-	for(int i=0;i<valuesPaths.size();i+=2){
-		cout<<valuesPaths[i].size()<<endl;
-		cout<<valuesPaths[i+1].size()<<endl;
-		cout<<endl;
-	}
-	Routing routing(doc,valuesPaths);
-	auto coordenadas=routing.calculate_route(100,360);
-		for(int i=0;i<coordenadas.size();i+=2){
-		cout<<coordenadas[i].size()<<endl;
-		cout<<coordenadas[i+1].size()<<endl;
-		cout<<endl;
-	}
 	
-	
+	if (doc.LoadFile("../arch_svg/wifiColores.svg")) {
+		cout << "\nFalla en la lectura del archivo!\n";
+		return 0;
+	}
 
+	int puntos[] = {2000, 2000, 2500, 3700, 1800, 300, 3605, 1123, 5642, 800};
+	int colores[] = {0xff0000, 0x0000ff};
+	int frames = 50;
+	float angulo = 120.0;
 
+	cout << "\nCarga del archivo completa!\nIniciando proceso de Selection...\n";
+	Selection Selection(puntos, 10, colores, 2, doc);
+	Selection.selectPaths();
+
+	cout << "\nProceso de Selection finalizado!\n";
+	vector<string> typePaths = Selection.getTypePath();
+	vector<vector<float>> valuesPaths = Selection.getPathValues();
+
+	vector<vector<float>> positionPaths;
+
+	Router* router = new Router(doc, valuesPaths, frames, angulo, &positionPaths);
+	Generator* generador = new Generator(doc, typePaths, &positionPaths);
+
+	Selection.attach(router);
+	router->attach(generador);
+
+	Selection.notify();
+
+	cout << "\nProceso Terminado\n";
+
+	delete router;
+	delete generador;
 
 	return 0;
 }
